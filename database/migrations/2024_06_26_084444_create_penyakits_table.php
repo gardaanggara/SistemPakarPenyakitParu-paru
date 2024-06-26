58b6c2a651bcf5f33d83302060fbc1f7d96cedd8
gardaanggara <shijhiho@gmail.com>
2024-06-26 15:59:31 +0700

    feat: prepare data

--- database/migrations/2024_06_26_084444_create_penyakits_table.php
+++ database/migrations/2024_06_26_084444_create_penyakits_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('penyakits', function (Blueprint $table) {
             $table->id();
-            $table->string('keterangan');
-            $table->timestamp('created_at');
+            $table->string('nama_penyakit');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('penyakits');
     }
 };
